e9fc9161d2c5742426163a3d584715dcfcd44651
uio23 <quantumTunnel56@icloud.com>
2023-05-06 13:17:48 +1200

    Remove redundant code, improve README

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Chase the Dot!
-#### Tello drone project
-An AR game where you try fly to virtual targets and score points.
+##### An AR game where you try fly to virtual targets and score points.
+If you happen to own a Tello drone, please do download an have a go playing yourself
+
+***
+####### Controls:
+**up arrow**: Forwards
+**down arrow**: Backwards
+**right arrow**: Right
+**left arrow**: Left
+**w key**: Up
+**s key**: Down
+**d key**: Yaw Right
+**a key**: Yaw Left
+
+**e key**: Take-off
+**q key**: Land
+
+**p key**: Screenshot
+***
+
+*Note: Still in development*
--- main.py
+++ main.py
@@ -15,15 +15,6 @@ from os import sys
 from target import Target
 
 
-pygame.init()
-
-
-# Initialize Tello drone
-drone = Tello()
-drone.connect()
-drone.streamon()
-
-
 # Pygame window parameters
 WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 720
@@ -33,31 +24,41 @@ WINDOW_NAME = 'Chase the Dot'
 # Derived experimentally (d°/s)
 DRONE_YAW_SPEED = 10.35
 
+# Use to store x & z displacment from frame-shift
+drone_x_z_change = [0, 0]
 
-# Initialize cv2 objects
+# Use as ~2s buffer before starting game after take-off
+CALIBRATION_DELAY = 150
+delayed = 0
 
-# Capture video from drone's udp port
-cap = cv2.VideoCapture(drone.get_udp_video_address())
+# target doesn't exist untill flying
+target = None
+# ORB feature detection alg. for speed + accuracy
 
-# ORB feature detection alg. -> speed + accuracy
 orb = cv2.ORB_create()
-
+# To store orb description of previous frame
+prev_frame_des = None
 # Define matching algorythm
 bruite_force_matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-dronePosition = np.zeros((3, 1))
-droneRotation = np.eye(3)
-prev_des = None
-old_pos = [0, 0, 0, 0]
+
+pygame.init()
+
+
+# Initialize Tello drone
+drone = Tello()
+drone.connect()
+drone.streamon()
+# Capture video from drone's udp port
+cap = cv2.VideoCapture(drone.get_udp_video_address())
+
 
 # Set up pygame variables
-font = pygame.font.Font(pygame.font.get_default_font(), 36)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(WINDOW_NAME)
-clock = pygame.time.Clock()
 
-# target object is empty untill take-off
-circle = None
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
+clock = pygame.time.Clock()
 
 
 def calculate_yaw_degree() -> float:
@@ -77,45 +78,40 @@ def calculate_yaw_degree() -> float:
 
     return yaw_degree
 
-calibration_delay = 0
+
 
 # Game-loop
 while True:
     success, frame = cap.read()
-    yaw_degree = 0
-
     if not success:
         break
+
 
     # Proccess frame for natural look
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     frame = cv2.flip(frame, 1)
 
-    if drone.is_flying and calibration_delay < 150:
-        calibration_delay += 1
 
-    if calibration_delay > 149:
+    # Execute ~2s buffer
+    if drone.is_flying and delayed < 150:
+        delayed += 1
+    # The game is active
+    if delayed >= CALIBRATION_DELAY:
         # If no target is present, create one
-        if not circle:
-            circle = Target(10, 60)
+        if not target:
+            target = Target(10, 60)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         kps, des = orb.detectAndCompute(gray, None)
-        if prev_des is not None:
+        if prev_frame_des is not None:
             # Find the 10% of the best description matches between the two frames
-            matches = sorted(bruite_force_matcher.match(des, prev_des), key=lambda match: match.distance)
+            matches = sorted(bruite_force_matcher.match(des, prev_frame_des), key=lambda match: match.distance)
             matches = matches[:int(len(matches)*0.1)]
 
-            dronePosition = np.zeros((3, 1))
-            droneRotation = np.eye(3)
-            dz = np.mean([kps[match.queryIdx].size - prev_kps[match.trainIdx].size for match in matches])
-            dy = np.mean([kps[match.queryIdx].pt[1] - prev_kps[match.trainIdx].pt[1] for match in matches])
-
-
-
             dx = np.mean([kps[match.queryIdx].pt[0] - prev_kps[match.trainIdx].pt[0] for match in matches])
-            dronePosition += droneRotation.dot(np.array([[dx], [dy], [dz]]))
+            dz = np.mean([kps[match.queryIdx].pt[1] - prev_kps[match.trainIdx].pt[1] for match in matches])
+            drone_x_z_change = [dx, dz]
 
             match_img = cv2.drawMatches(frame, kps, prev_frame, prev_kps, matches, None)
             img_surface = pygame.surfarray.make_surface(match_img)
@@ -125,7 +121,7 @@ while True:
 
         # Record current frame features for comparison
         prev_kps = kps
-        prev_des = des
+        prev_frame_des = des
         prev_frame = frame
 
         velocity = [0, 0, 0, 0]
@@ -148,6 +144,8 @@ while True:
         elif keys[pygame.K_a]:
             velocity[3] = -60
             yaw_degree = calculate_yaw_degree()
+        else:
+            yaw_degree = 0
         drone.send_rc_control(velocity[0], velocity[1], velocity[2], velocity[3])
     else:
         img_surface = pygame.surfarray.make_surface(frame)
@@ -166,41 +164,25 @@ while True:
                 frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
                 frame = cv2.flip(frame, 1)
                 timestr = time.strftime('%H-%M', time.localtime())
-                cv2.imwrite(f'./images/Tello-{timestr}.jpg', frame)
-                pygame.image.save(screen, f'./images/Game-{timestr}.jpeg')
-            elif event.key == pygame.K_i:
-                print(f'\nBattery: {drone.get_battery}\n')
-
-
-    # Drone-free option
-    #screen.fill((255, 255, 255))
-    #circle.draw(screen)
+                cv2.imwrite(f'./Tello-{timestr}.jpg', frame)
+                pygame.image.save(screen, f'./Game-{timestr}.jpeg')
 
 
     # Display the current frame
     screen.blit(img_surface, (0, 0))
 
 
-    # If there is a circle, prepare (y & d) change values
-    if circle:
-        try:
-            pos = [int(number/1) for number in dronePosition.flatten()]
-        except:
-            pass
-        for i, val in enumerate(pos):
-            pos[i] -= old_pos[i]
-
-
-        # Update target circle's position proportional to
+    if target:
+        # Update target target's position proportional to
         # (x & z) frame displacment, y velocity and calculated d
-        circle.update(pos[1]/4, -velocity[1]/20, pos[0]/4, yaw_degree)
+        target.update(drone_x_z_change[1]/4, -velocity[1]/20, drone_x_z_change[0]/4, yaw_degree)
 
         # Render HUD
-        x_distance_display = font.render(f'X - distance: {round(circle.distance[0], 2)}', True, (0, 0, 250))
-        y_distance_display = font.render(f'Y - distance: {round(circle.distance[1], 2)}', True, (0, 0, 250))
-        z_distance_display = font.render(f'Z - distance: {round((circle.distance[2]*-1), 2)}', True, (0, 0, 250))
+        x_distance_display = font.render(f'X - distance: {round(target.distance[0], 2)}', True, (0, 0, 250))
+        y_distance_display = font.render(f'Y - distance: {round(target.distance[1], 2)}', True, (0, 0, 250))
+        z_distance_display = font.render(f'Z - distance: {round((target.distance[2]*-1), 2)}', True, (0, 0, 250))
 
-        score_display = font.render(f'Score: {circle.score}', True, (0, 250, 0))
+        score_display = font.render(f'Score: {target.score}', True, (0, 250, 0))
 
 
         # Blit HUD
@@ -209,8 +191,8 @@ while True:
         screen.blit(z_distance_display, dest=(10,75))
         screen.blit(score_display, dest=(800,10))
 
-        # Blit target circle
-        circle.draw(screen)
+        # Blit target
+        target.draw(screen)
 
 
     # Update the whole display and limit frame-rate
